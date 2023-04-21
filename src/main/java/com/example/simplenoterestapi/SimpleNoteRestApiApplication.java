1c926a716ba32780bb7382ca27c0cf16bea6b898
Kha Tran <khadieptran@gmail.com>
2023-04-21 02:00:21 -0400

    Set up project with intial dependencies

---
+++ src/main/java/com/example/simplenoterestapi/SimpleNoteRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplenoterestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleNoteRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleNoteRestApiApplication.class, args);
+    }
+
+}
